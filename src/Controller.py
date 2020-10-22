d8e5b8b5f97c3e8e429a5345b5164d895e78fbc6
Arjun Gandhi <arjungandhi06@gmail.com>
2020-10-21 20:50:44 -0400

    added framework for controller super class and empty kinematics class

---
+++ src/Controller.py
@@ -0,0 +1,25 @@
+class Controller: 
+    
+    def __init__(self):
+        raise NotImplementedError
+
+    def send_pos(self,vals):
+        raise NotImplementedError
+    
+    def get_pos(self):
+        raise NotImplementedError
+    
+
+    def send_vel(self,vals):
+        raise NotImplementedError
+    
+    def get_vel(self):
+        raise NotImplementedError
+    
+    def send_torque(self,vals):
+        raise NotImplementedError
+    
+    def get_torque(self):
+        raise NotImplementedError
+    
+    
